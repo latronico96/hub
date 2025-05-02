64f0d72aa78479814344ae1d5a6f2d314f3d2773
latronico96 <35084047+latronico96@users.noreply.github.com>
2025-05-02 00:26:04 -0300

    corregir api de totales usando el cache

--- recetario/urls.py
+++ recetario/urls.py
@@ -1,10 +1,13 @@
+from django.urls import path
 from rest_framework.routers import DefaultRouter
 
-from .views import ProductoViewSet, RecetaViewSet, UnidadViewSet
+from .views import ProductoViewSet, RecetaViewSet, UnidadViewSet, DashboardView
 
 router = DefaultRouter()
 router.register(r"unidades", UnidadViewSet, basename="unidades")
 router.register(r"productos", ProductoViewSet, basename="productos")
 router.register(r"recetas", RecetaViewSet, basename="recetas")
 
-urlpatterns = router.urls
+urlpatterns = [
+    path('dashboard/totales/', DashboardView.as_view(), name='dashboard-totales'),
+] + router.urls
