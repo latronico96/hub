8eb2e41e9db555ca3521ac753a488485f7734750
latronico96 <35084047+latronico96@users.noreply.github.com>
2025-12-16 21:09:41 -0300

    agregar urls

--- users/urls.py
+++ users/urls.py
@@ -1,13 +1,20 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 
-from .views import MercadoPagoIPNView, PermissionViewSet, UserViewSet
+from .views import (
+    MercadoPagoIPNView,
+    PermissionViewSet,
+    UserViewSet,
+    ImportDatabaseView,
+    ExportDatabaseView,
+)
 
 router = DefaultRouter()
 router.register(r"permissions", PermissionViewSet, basename="permission")
 router.register(r"", UserViewSet, basename="user")
 
-
 urlpatterns = [
     path("mercadopago/ipn/", MercadoPagoIPNView.as_view(), name="mercadopago-ipn"),
+    path("import-db/", ImportDatabaseView.as_view(), name="import-database"),
+    path("export-db/", ExportDatabaseView.as_view(), name="export-database"),
 ] + router.urls
